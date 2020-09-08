84ee36da18e6de1f9fcd7596c1eb8c75b374c9db
Andrew.Petin <Andrew.Petin@visionblue.onmicrosoft.com>
2020-09-08 17:46:58 +0300

    add bundle to VCS

---
+++ build/precache-manifest.b5c4fef9f809f5f46d5c1f11cfce1cea.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1662531c2284425136e778f2487772",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19851e44b3867a731d05",
+    "url": "/static/js/bundle.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/bundle.js.LICENSE.txt"
+  }
+]);
